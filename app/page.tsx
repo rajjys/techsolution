ecbcd762bf8f18f9fc6cfcec06f431ccf5a618c2
rajjys <bisingwaj@gmail.com>
2026-08-01 15:33:19 +0100

    Comment out KitsShowcase component in HomePage for future consideration.

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
       <CaseStudiesReach />
       <SolarStatement />
       <KitsSelector />
-      <KitsShowcase />
+      {/* <KitsShowcase /> */}
     </>
   );
 }
